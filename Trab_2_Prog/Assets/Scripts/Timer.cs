d1e808d8fcf7be98f0eac5f8f640ce45057d9c06
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2020-12-03 23:12:30 -0300

    Final Commit

---
+++ Trab_2_Prog/Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+
+    private float _timeStamp = 0f;
+
+    private void OnEnable()
+    {
+        _timeStamp = 0f;
+        
+    }
+
+    private void Update()
+    {
+        TimeSpan time = TimeSpan.FromSeconds(_timeStamp);
+        _text.text = time.ToString("mm':'ss");
+        _timeStamp += Time.deltaTime;
+    }
+
+    private void OnValidate()
+    {
+        if (_text == null)
+        {
+            _text = this.GetComponent<TextMeshProUGUI>();
+        }
+    }
+}
